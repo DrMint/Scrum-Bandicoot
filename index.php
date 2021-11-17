8736ac76e42cca1af37873b640c35e9dc013a82e
DrMint <thomas.barillot@etu.u-bordeaux.fr>
2021-11-17 22:24:59 +0100

    Added groundwork for the project page

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
-<?php
-  // Authentification check
-  if (session_status() == PHP_SESSION_NONE) session_start();
-  if (!isset($_SESSION['loginUsername'])) {
-    header('Location: /login');
-  }
- ?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="/css/master.css">
-    <title>Scrum Bandicoot</title>
+    <link rel="stylesheet" href="/css/home.css">
+    <title>Scrum Bandicoot - Home</title>
   </head>
   <body>
 
-    <h1>Hello <?php echo $_SESSION['loginUsername']; ?></h1>
+    <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/templates/navbar.php") ?>
+
+    <div class="container">
+      <div id="projects">
+        <div id="projectsHeader">
+          <h2>My projects</h2>
+          <a class="button outline" href="/projects/create">Create a project</a>
+          <a id="joinProject" class="button outline" href="/projects/join">Join a project</a>
+        </div>
+      </div>
+    </div>
 
   </body>
 </html>
--- templates/navbar.php
+++ templates/navbar.php
@@ -1,5 +1,4 @@
-<link rel="stylesheet" href="/css/admin/adminbar.css">
-<link rel="stylesheet" href="/css/fontawesome.css">
+<link rel="stylesheet" href="/css/navbar.css">
 
 <?php require_once($_SERVER["DOCUMENT_ROOT"] . "/tools/users.php") ?>
 
@@ -12,22 +11,15 @@
  ?>
 
  <?php
-
     function isActive($url) {
       return $url === dirname($_SERVER["PHP_SELF"]);
     }
-
   ?>
 
-<div id="admin-bar">
-  <h1>Accord's CMS</h1>
-  <a class="button icon invert <?php if (isActive('/admin/pages')) echo 'active'; ?>" href="/admin/pages"><i class="fa-solid fa-file-lines"></i></a>
-  <a class="button icon invert <?php if (isActive('/admin/users')) echo 'active'; ?>" href="/admin/users"><i class="fa-solid fa-user"></i></a>
-  <a class="button icon invert <?php if (isActive('/admin/comments')) echo 'active'; ?>" href="/admin/comments"><i class="fa-solid fa-comment"></i></a>
-  <a class="button icon invert <?php if (isActive('/admin/scripts')) echo 'active'; ?>" href="/admin/scripts"><i class="fa-solid fa-code"></i></a>
-  <a class="button icon invert <?php if (isActive('/admin/settings')) echo 'active'; ?>" href="/admin/settings"><i class="fa-solid fa-gear"></i></a>
+<div id="navbar">
+  <h1>Srum Bandicoot</h1>
   <div id="logout">
-    <p>Welcome <?php echo getCurrentUser()->name; ?></p>
-    <a class="button invert outline" href="/admin/logout.php">Logout</a>
+    <p>Welcome <?php echo getCurrentUser()->slug; ?></p>
+    <a class="button invert outline" href="/logout">Logout</a>
   </div>
 </div>
