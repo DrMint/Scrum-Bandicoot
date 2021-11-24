ce4c92d31a9ce60fc269b71fd90e1892d7f88c3b
Thomas Barillot <thomas.barillot@etu.u-bordeaux.fr>
2021-11-24 13:33:10 +0100

    Added two UT and the Cahier des tests

---
+++ TESTING.md
@@ -0,0 +1,4 @@
+|  id  | Type  | Actions  | Attendu  | Résultats  | Commentaire  | Date  | Testeur  |
+| ---- | ------- | ----------- | ----------- | ------------- | ------------------- | ------- | ----------- |
+|  1  |  TU  |  users.createUser() |  Creation d'un object User dont le nom est 'ut-test'  |  OK  |   | 2021-11-24 13:25:59  |  N/A  |
+|  2  |   TU  | users.retrieveExistingUser()  |  Création d'un object User avec un nom existant. Verification que le hash utilisateur est celui attendu  | OK  |   | 2021-11-24 13:29:36  |  N/A  |
---
+++ ut/users.php
@@ -0,0 +1,44 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/tools/users.php");
+    
+    function assertTrue($assertion) {
+        if ($assertion) {
+            return 'pass';
+        } else {
+            return 'failed';
+        }
+    }
+
+    function createUser() {
+        $username = 'ut-test';
+        $user = new User($username);
+        return assertTrue($user->slug === $username);
+    }
+
+    function retrieveExistingUser() {
+        global $rootFolder;
+
+        // Make sure the user ut-test exists
+        $username = 'ut-test';
+        $hash = '123456789';
+        $filePath = $rootFolder . $username . '.json';;
+        $file = fopen($filePath, 'w');
+        fwrite($file, json_encode( ['hash' => $hash] ));
+        fclose($file);
+
+        // Retrieve it
+        $user = new User($username);
+
+        // The retrieved hash should be equal to the one we've set
+        return assertTrue($user->hash === $hash);
+    }
+
+    echo 'createUser: ' . createUser() . '<br>';
+    echo 'retrieveExistingUser: ' . retrieveExistingUser() . '<br>';
+
+ ?>
